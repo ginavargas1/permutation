e8151577a9d6a75c6a00ba90096dbde267570e08
Gina Vargas <gina.vargas89@outlook.com>
2021-06-23 14:16:37 -0700

    adding different elements

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # permutation
+
+Keep function pure. 
+
--- script.js
+++ script.js
@@ -1,16 +1,36 @@
 // Heap's algorithm 
 // using an array and making a pure function 
+//create two function 1. to swaps the element (this wll get an array index A and B)
 
 const getPermutations = arr => {
+
+    const output = [];
 
     const swapInPlace = (arrToSwap, indexA, indexB) => {
         const temp = arrToSwap[indexA];
         arrToSwap[indexA] = arrToSwap[indexB]
         arrToSwap[indexB] = temp
+    };
+
+    const generate = (n, heapArry) => {
+        if (n === 1) {
+            output.push(heapArry.slice());
+            return;
+        }
+
+        generate(n - 1, heapArry);
+
+        for (let i = 0; i < n -1; i++) {
+            if (n % 2 === 0) {
+                swapInPlace
+            }
+            
+        }
     }
+
 
 }
 
-//const array = [1,2,3]
+//const array = [1,2,3] 
 //getPermutation(arr);
 //console.log(arr)

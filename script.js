739befb4d07eee6bb550ee387e52bf45bc941a63
Gina Vargas <gina.vargas89@outlook.com>
2021-06-23 13:47:34 -0700

    details of the algorithm

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-// using 123
+// Heap's algorithm 
+// using an array and making a pure function 
+
+const getPermutations = arr => {
+
+    const swapInPlace = (arrToSwap, indexA, indexB) => {
+        const temp = arrToSwap[indexA];
+        arrToSwap[indexA] = arrToSwap[indexB]
+        arrToSwap[indexB] = temp
+    }
+
+}
+
+//const array = [1,2,3]
+//getPermutation(arr);
+//console.log(arr)
